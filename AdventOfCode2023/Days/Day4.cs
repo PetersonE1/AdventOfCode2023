711a1454b83688a0334bf5914fb8c796c62f7237
PetersonE1 <PetersonE_1@live.com>
2023-12-08 15:40:12 -0500

    day 4 part 2

--- AdventOfCode2023/Days/Day4.cs
+++ AdventOfCode2023/Days/Day4.cs
@@ -14,6 +14,7 @@ namespace AdventOfCode2023.Days
         public static void Run(string input)
         {
             Console.WriteLine(Part1(input));
+            Console.WriteLine(Part2(input));
         }
 
         static int Part1(string input)
@@ -43,6 +44,43 @@ namespace AdventOfCode2023.Days
             }
 
             return totalSum;
+        }
+
+        static int Part2(string input)
+        {
+            string[][] lines = input.Split("\r\n").Select(x => x.Split('|')).ToArray();
+            List<Card> cards = new List<Card>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] line = lines[i];
+                line[0] = line[0].Substring(line[0].IndexOf(':'));
+                int count = 0;
+                Match[] matches = numberRgx.Matches(line[0]).ToArray();
+                foreach (Match match in numberRgx.Matches(line[1]))
+                    if (matches.Where(m => m.Value == match.Value).Count() >= 1)
+                        count++;
+
+                Card card = new Card() { Indices = new int[count] };
+                for (int j = 1; j <= count; j++)
+                    card.Indices[j-1] = i + j;
+                cards.Add(card);
+            }
+
+            List<Card> cardsIterator = new List<Card>(cards);
+            for (int i = 0; i < cardsIterator.Count; i++)
+            {
+                Card card = cardsIterator[i];
+                foreach (int index in card.Indices)
+                    cardsIterator.Add(cards[index]);
+            }
+
+            return cardsIterator.Count;
+        }
+
+        class Card
+        {
+            public int[] Indices { get; set; }
         }
     }
 }
