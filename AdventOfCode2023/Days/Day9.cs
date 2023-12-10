fdbc206cb37afb3d617d769e9505d7e110d024fa
PetersonE1 <PetersonE_1@live.com>
2023-12-10 01:43:04 -0500

    day 9 part 2

--- AdventOfCode2023/Days/Day9.cs
+++ AdventOfCode2023/Days/Day9.cs
@@ -12,6 +12,7 @@ namespace AdventOfCode2023.Days
         public static void Run(string input)
         {
             Console.WriteLine(Part1(input));
+            Console.WriteLine(Part2(input));
         }
 
         static long Part1(string input)
@@ -24,6 +25,16 @@ namespace AdventOfCode2023.Days
             return sum;
         }
 
+        static long Part2(string input)
+        {
+            string[] lines = input.Split("\r\n");
+            long sum = 0;
+            foreach (string line in lines)
+                sum += GetPrevious(line.Split(' ').Select(x => int.Parse(x)).ToArray());
+
+            return sum;
+        }
+
         static int GetNext(int[] inputs)
         {
             int[] diffs = new int[inputs.Length - 1];
@@ -32,6 +43,16 @@ namespace AdventOfCode2023.Days
             if (diffs.All(x => x == 0))
                 return inputs[^1];
             return inputs[^1] + GetNext(diffs);
+        }
+
+        static int GetPrevious(int[] inputs)
+        {
+            int[] diffs = new int[inputs.Length - 1];
+            for (int i = 0; i < inputs.Length - 1; i++)
+                diffs[i] = inputs[i + 1] - inputs[i];
+            if (diffs.All(x => x == 0))
+                return inputs[0];
+            return inputs[0] - GetPrevious(diffs);
         }
     }
 }
