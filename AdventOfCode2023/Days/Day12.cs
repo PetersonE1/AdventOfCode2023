ca4ffa20e3c7b30ca3db29b13320539356c8efdb
PetersonE1 <PetersonE_1@live.com>
2024-03-22 10:39:00 -0400

    set up for day 12

---
+++ AdventOfCode2023/Days/Day12.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Days
+{
+    internal static class Day12
+    {
+        public static void Run(string input)
+        {
+            Console.WriteLine(Part1(input));
+        }
+
+        private static int Part1(string input)
+        {
+            return 0;
+        }
+    }
+}
