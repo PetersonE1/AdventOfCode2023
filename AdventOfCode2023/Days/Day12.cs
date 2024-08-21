b602ab2125b6b294c81f8485b246c3ab38a3269d
CF-Code-Collective-Admin <cf-code-collective@outlook.com>
2024-08-21 13:53:00 -0400

    indentify and remove confirmed sequences

--- AdventOfCode2023/Days/Day12.cs
+++ AdventOfCode2023/Days/Day12.cs
@@ -22,7 +22,6 @@ namespace AdventOfCode2023.Days
             // Generate count of possibilities per region, multiply all together to get possibilities per line
 
             input = "???.### 1,1,3";
-
             (string sequence, int[] lengths)[] lines = input.Split("\r\n").Select(n => {
                 var arr = n.Split(' ');
                 return (arr[0], arr[1].Split(',').Select(r => int.Parse(r)).ToArray());
@@ -35,7 +34,37 @@ namespace AdventOfCode2023.Days
 
         private static void IdentifyConfirmedDamagedSequences(ref (string sequence, int[] lengths) input)
         {
-            
+            StringBuilder builder = new();
+
+            List<int> lengths = input.lengths.ToList();
+
+            for (int index = 0; index < lengths.Count; index++)
+            {
+                int length = lengths[index];
+                builder.Clear();
+                for (int i = 0; i < length; i++){
+                    builder.Append('#');
+                }
+                string checker = builder.ToString();
+
+                if (Contains(input.sequence, checker))
+                {
+                    input.sequence = input.sequence.Replace(checker, string.Empty);
+                    lengths.RemoveAt(index);
+                    index--;
+                }
+            }
+        }
+
+        private static bool Contains(string source, string substring)
+        {
+            if (source.Contains('.' + substring + '.'))
+                return true;
+            if (string.Concat(source.Take(substring.Length + 1)) == substring + '.')
+                return true;
+            if (string.Concat(source.TakeLast(substring.Length + 1)) == '.' + substring)
+                return true;
+            return false;
         }
     }
 }
