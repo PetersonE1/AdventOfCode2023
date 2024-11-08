308ac6b3fe729439d573749f3115c6facb39ad56
CF-Code-Collective-Admin <cf-code-collective@outlook.com>
2024-11-08 15:17:49 -0500

    day 12 part 1

--- AdventOfCode2023/Days/Day12.cs
+++ AdventOfCode2023/Days/Day12.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +15,7 @@ namespace AdventOfCode2023.Days
             Console.WriteLine(Part1(input));
         }
 
-        private static int Part1(string input)
+        private static ulong Part1(string input)
         {
             // Remove confirmed sequences of damaged springs from the input list
             // Attempt to logically confirm unknowns next to confirmed damages
@@ -21,50 +23,188 @@ namespace AdventOfCode2023.Days
             // Iterate over regions, starting leftmost and moving right, nesting for multiple list numbers
             // Generate count of possibilities per region, multiply all together to get possibilities per line
 
-            input = "???.### 1,1,3";
-            (string sequence, int[] lengths)[] lines = input.Split("\r\n").Select(n => {
+            // Method 2: Treat as binary, count from 0 up and perform a bitwise AND to check if it fits the pattern
+            // If it fits, group and check if it matches the numbers. If so, increment counter by 1
+
+            // Method 2 works for part 1. Will not work for part 2. Need to be smarter.
+
+            (Boolish[] sequence, int[] lengths)[] lines = input.Split("\r\n").Select(n => {
                 var arr = n.Split(' ');
-                return (arr[0], arr[1].Split(',').Select(r => int.Parse(r)).ToArray());
+                return (ToBoolish(arr[0]), arr[1].Split(',').Select(r => int.Parse(r)).ToArray());
             }).ToArray();
 
-            IdentifyConfirmedDamagedSequences(ref lines[0]);
+            int lineCount = 0;
+            ulong totalCount = 0;
+            foreach (var line in lines)
+            {
+                lineCount++;
+                if (lineCount % 100 == 0)
+                    Console.WriteLine($"Processing line {lineCount}");
 
-            return 0;
+                BitArray bitArray = new BitArray(line.sequence.Length);
+
+                int topBound = (int)Math.Pow(2, bitArray.Count) - 1;
+
+                uint count = 0;
+                for (int i = 0; i < topBound; i++)
+                {
+                    Increment(bitArray);
+                    if (BitwiseANDCheck(bitArray, line.sequence))
+                    {
+                        bool[] bools = new bool[bitArray.Count];
+                        bitArray.CopyTo(bools, 0);
+                        bool[][] grouped = bools.GroupConsecutive().ToArray();
+                        if (grouped.Length != line.lengths.Length)
+                            continue;
+                        bool countUp = true;
+                        for (int j = 0; j < line.lengths.Length; j++)
+                        {
+                            if (grouped[j].Length != line.lengths[j])
+                            {
+                                countUp = false;
+                                break;
+                            }
+                        }
+                        if (countUp)
+                            count++;
+                    }
+                }
+                totalCount += count;
+            }
+
+            return totalCount;
         }
 
-        private static void IdentifyConfirmedDamagedSequences(ref (string sequence, int[] lengths) input)
+        public static void Increment(BitArray bArray)
         {
-            StringBuilder builder = new();
-
-            List<int> lengths = input.lengths.ToList();
-
-            for (int index = 0; index < lengths.Count; index++)
+            for (int i = 0; i < bArray.Count; i++)
             {
-                int length = lengths[index];
-                builder.Clear();
-                for (int i = 0; i < length; i++){
-                    builder.Append('#');
-                }
-                string checker = builder.ToString();
-
-                if (Contains(input.sequence, checker))
+                bool previous = bArray[i];
+                bArray[i] = !previous;
+                if (!previous)
                 {
-                    input.sequence = input.sequence.Replace(checker, string.Empty);
-                    lengths.RemoveAt(index);
-                    index--;
+                    // Found a clear bit - now that we've set it, we're done
+                    return;
                 }
             }
         }
 
-        private static bool Contains(string source, string substring)
+        public static Boolish[] ToBoolish(string sequence)
         {
-            if (source.Contains('.' + substring + '.'))
-                return true;
-            if (string.Concat(source.Take(substring.Length + 1)) == substring + '.')
-                return true;
-            if (string.Concat(source.TakeLast(substring.Length + 1)) == '.' + substring)
-                return true;
+            Boolish[] boolishes = new Boolish[sequence.Length];
+            for (int i = 0; i < boolishes.Length; i++)
+            {
+                switch (sequence[i])
+                {
+                    case '#': boolishes[i] = new(Boolish.BoolishValue.True); break;
+                    case '.': boolishes[i] = new(Boolish.BoolishValue.False); break;
+                    case '?': boolishes[i] = new(Boolish.BoolishValue.True | Boolish.BoolishValue.False); break;
+                    default: throw new ArgumentException($"Invalid input char: {sequence[i]}");
+                }
+            }
+
+            return boolishes;
+        }
+
+        public static bool BitwiseANDCheck(BitArray a, Boolish[] b)
+        {
+            if (a.Length != b.Length)
+                throw new ArgumentException("Arrays are not the same length");
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                    return false;
+            }
+            return true;
+        }
+
+        public static IEnumerable<bool[]> GroupConsecutive(this IEnumerable<bool> list)
+        {
+            if (list.Any())
+            {
+                int index = 0;
+                bool rootItem = list.First();
+
+                for (int i = 1; i < list.Count(); i++)
+                {
+                    if (!list.ElementAt(i).Equals(rootItem))
+                    {
+                        // Only yields the damaged sequences
+                        if (rootItem)
+                            yield return list.Take(new Range(index, i)).ToArray();
+                        index = i;
+                        rootItem = list.ElementAt(i);
+                    }
+                }
+                if (rootItem)
+                    yield return list.Take(new Range(index, list.Count())).ToArray();
+            }
+        }
+    }
+
+    public class Boolish
+    {
+        [Flags]
+        public enum BoolishValue
+        {
+            True = 1,
+            False = 2
+        }
+
+        public BoolishValue Value { get; set; }
+
+        public Boolish(BoolishValue value)
+        {
+            Value = value;
+        }
+
+        public Boolish(bool value)
+        {
+            Value = value ? BoolishValue.True : BoolishValue.False;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Boolish)
+            {
+                Boolish other = (Boolish)obj;
+                return (this.Value.HasFlag(BoolishValue.True) && other.Value.HasFlag(BoolishValue.True))
+                || (this.Value.HasFlag(BoolishValue.False) && other.Value.HasFlag(BoolishValue.False));
+            }
+            if (obj is bool)
+            {
+                bool other = (bool)obj;
+                return (this.Value.HasFlag(BoolishValue.True) && other) || (this.Value.HasFlag(BoolishValue.False) && !other);
+            }
             return false;
         }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            if (Value.HasFlag(BoolishValue.True))
+            {
+                if (Value.HasFlag(BoolishValue.False))
+                    return "Maybe";
+                return "True";
+            }
+            if (Value.HasFlag(BoolishValue.False))
+                return "False";
+            return "Empty";
+        }
+
+        public static bool operator ==(Boolish a, Boolish b) => a.Equals(b);
+        public static bool operator !=(Boolish a, Boolish b) => !a.Equals(b);
+
+        public static bool operator ==(Boolish a, bool b) => a.Equals(b);
+        public static bool operator !=(Boolish a, bool b) => !a.Equals(b);
+
+        public static bool operator ==(bool a, Boolish b) => b.Equals(a);
+        public static bool operator !=(bool a, Boolish b) => !b.Equals(a);
     }
 }
