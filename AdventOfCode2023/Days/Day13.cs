8508fcf3f0e1a6aafc8e00c3023fb0803f4f4246
CF-Code-Collective-Admin <cf-code-collective@outlook.com>
2024-11-18 16:02:29 -0500

    first attempt, close but not quite

--- AdventOfCode2023/Days/Day13.cs
+++ AdventOfCode2023/Days/Day13.cs
@@ -11,49 +11,118 @@ namespace AdventOfCode2023.Days
             Console.WriteLine(Part1(input));
         }
 
-        // Error: Checking for any reflection at all, need perfect reflection. Return to original list idea.
-        // What counts as perfect reflection? In the first "proper" shape, the top three (3) rows are ignored. Longest reflection?
+        // Perfect reflection => One side of the reflection must touch a border
+
+        // Reflections getting dropped in current implementation: An imperfect reflection gets caught, discarded,
+        // and a subsequent perfect reflection is skipped
+
         private static ulong Part1(string input)
         {
             string[][] grids = input.Split("\r\n\r\n").Select(s => s.Split("\r\n")).ToArray();
 
             ulong count = 0;
 
+            int index = 0;
             foreach (string[] grid in grids)
             {
-                string cachedString = grid[0];
-                bool doBreak = false;
-                for (uint i = 1; i < grid.Length; i++)
-                {
-                    if (grid[i] == cachedString)
-                    {
-                        count += i;
-                        doBreak = true;
-                        break;
-                    }
-                    cachedString = grid[i];
-                }
+                index++;
+                var rowMirror = GetMirror(grid, GetRow);
+                var colMirror = GetMirror(grid, GetColumn, true);
 
-                if (doBreak) break;
-                cachedString = grid.GetColumn(0);
-                for (int i = 1; i < grid.Length; i++)
+                count += 100 * (uint)rowMirror.index + (uint)colMirror.index;
+
+                if (rowMirror.index - rowMirror.length > 0 && rowMirror.index + rowMirror.length < grid.GetColumn(0).Length)
+                    throw new Exception($"ERROR ERROR IN ROW: [index={rowMirror.index},length={rowMirror.length},tot={grid.GetColumn(0).Length}]");
+                if (colMirror.index - colMirror.length > 0 && colMirror.index + colMirror.length < grid[0].Length)
+                    throw new Exception($"ERROR ERROR IN COL: [index={colMirror.index},length={colMirror.length},tot={grid[0].Length}]");
+
+                Console.WriteLine("passed");
+
+                // DEBUG DISPLAY
+                if (index % 20 == 0)
                 {
-                    string col = grid.GetColumn(i);
-                    if (col == cachedString)
+                    foreach (string s in grid) Console.WriteLine(s);
+                    Console.WriteLine($"Horizontal Mirror: {(rowMirror.length > 0 ? $"Found at index {rowMirror.index}, length {rowMirror.length}" : "Not found.")}");
+                    Console.WriteLine($"Vertical Mirror: {(colMirror.length > 0 ? $"Found at index {colMirror.index}, length {colMirror.length}" : "Not found.")}");
+                    Console.WriteLine();
+                    if (rowMirror.length == 0)
                     {
-                        count += (uint)i * 100;
-                        break;
+                        Console.WriteLine($"ZERO CHECK: {rowMirror.index}");
                     }
-                    cachedString = col;
                 }
             }
 
             return count;
         }
 
-        private static string GetColumn(this string[] grid, int index)
+        /*private static ulong Part1B(string input)
         {
-            return grid.Aggregate(string.Empty, (result, current) => result + current[index]);
+            BitArray[][] grids = input.Split("\r\n\r\n").Select(s => s.Split("\r\n").Select(s => {
+                BitArray b = new(s.Length);
+                for (int i = 0; i < s.Length; i++)
+                    b.Set(i, s[i] == '#' ? true : false);
+                return b;
+            }).ToArray()).ToArray();
+
+            ulong count = 0;
+            foreach (BitArray[] grid in grids)
+            {
+                
+            }
+
+            return count;
+        }*/
+
+        private static string GetColumn(this string[] grid, int index) => grid.Aggregate(string.Empty, (result, current) => result + current[index]);
+
+        private static string GetRow(this string[] grid, int index) => grid[index];
+
+        private delegate string Selector(string[] grid, int index);
+        private static (uint length, int index) GetMirror(string[] grid, Selector selector, bool isCol = false)
+        {
+            (uint length, int index) mirror = new();
+
+            List<string> lines = [selector(grid, 0)];
+            bool isReversing = false;
+            int length = isCol ? grid[0].Length : grid.Length;
+            for (int i = 1; i < length; i++)
+            {
+                if (lines.Count == 0) break;
+                if (!isReversing)
+                {
+                    if (selector(grid, i) == lines[^1])
+                    {
+                        mirror.index = i;
+                        mirror.length = 1;
+                        lines.RemoveAt(lines.Count - 1);
+                        isReversing = true;
+                        continue;
+                    }
+                    lines.Add(selector(grid, i));
+                }
+                else
+                {
+                    if (selector(grid, i) != lines[^1])
+                    {
+                        mirror.index = 0;
+                        mirror.length = 0;
+                        break;
+                    }
+                    mirror.length++;
+                    lines.RemoveAt(lines.Count - 1);
+                }
+            }
+
+            return mirror;
+        }
+
+        private static bool HasOneSet(this BitArray arr)
+        {
+            int count = 0;
+            foreach (bool b in arr)
+                if (b) count++;
+
+            return count == 1 ? true : false;
         }
     }
 }
