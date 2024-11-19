4e467ed17eb4c979e1a1a6dba65b60e8f3d179d3
CF-Code-Collective-Admin <cf-code-collective@outlook.com>
2024-11-18 20:30:47 -0500

    day 13, part 1

--- AdventOfCode2023/Days/Day13.cs
+++ AdventOfCode2023/Days/Day13.cs
@@ -9,120 +9,45 @@ namespace AdventOfCode2023.Days
         public static void Run(string input)
         {
             Console.WriteLine(Part1(input));
+            Console.WriteLine(Part2(input));
         }
 
         // Perfect reflection => One side of the reflection must touch a border
-
-        // Reflections getting dropped in current implementation: An imperfect reflection gets caught, discarded,
-        // and a subsequent perfect reflection is skipped
 
         private static ulong Part1(string input)
         {
             string[][] grids = input.Split("\r\n\r\n").Select(s => s.Split("\r\n")).ToArray();
 
             ulong count = 0;
-
-            int index = 0;
+            
             foreach (string[] grid in grids)
             {
-                index++;
-                var rowMirror = GetMirror(grid, GetRow);
-                var colMirror = GetMirror(grid, GetColumn, true);
-
-                count += 100 * (uint)rowMirror.index + (uint)colMirror.index;
-
-                if (rowMirror.index - rowMirror.length > 0 && rowMirror.index + rowMirror.length < grid.GetColumn(0).Length)
-                    throw new Exception($"ERROR ERROR IN ROW: [index={rowMirror.index},length={rowMirror.length},tot={grid.GetColumn(0).Length}]");
-                if (colMirror.index - colMirror.length > 0 && colMirror.index + colMirror.length < grid[0].Length)
-                    throw new Exception($"ERROR ERROR IN COL: [index={colMirror.index},length={colMirror.length},tot={grid[0].Length}]");
-
-                Console.WriteLine("passed");
-
-                // DEBUG DISPLAY
-                if (index % 20 == 0)
-                {
-                    foreach (string s in grid) Console.WriteLine(s);
-                    Console.WriteLine($"Horizontal Mirror: {(rowMirror.length > 0 ? $"Found at index {rowMirror.index}, length {rowMirror.length}" : "Not found.")}");
-                    Console.WriteLine($"Vertical Mirror: {(colMirror.length > 0 ? $"Found at index {colMirror.index}, length {colMirror.length}" : "Not found.")}");
-                    Console.WriteLine();
-                    if (rowMirror.length == 0)
-                    {
-                        Console.WriteLine($"ZERO CHECK: {rowMirror.index}");
-                    }
-                }
+                for (int i = 1; i < grid[0].Length; i++)
+                    if (grid.IsMirror(GetColumn, i - 1, true)) count += (uint)i;
+                
+                for (int i = 1; i < grid.Length; i++)
+                    if (grid.IsMirror(GetRow, i - 1)) count += 100 * (uint)i;
             }
 
             return count;
         }
 
-        /*private static ulong Part1B(string input)
+        private static ulong Part2(string input)
         {
-            BitArray[][] grids = input.Split("\r\n\r\n").Select(s => s.Split("\r\n").Select(s => {
-                BitArray b = new(s.Length);
-                for (int i = 0; i < s.Length; i++)
-                    b.Set(i, s[i] == '#' ? true : false);
-                return b;
-            }).ToArray()).ToArray();
+            throw new NotImplementedException();
+        }
 
-            ulong count = 0;
-            foreach (BitArray[] grid in grids)
-            {
-                
-            }
-
-            return count;
-        }*/
-
-        private static string GetColumn(this string[] grid, int index) => grid.Aggregate(string.Empty, (result, current) => result + current[index]);
+        private static string GetColumn(this string[] grid, int index) => grid.Aggregate(string.Empty, (result, current) => result += current[index]);
 
         private static string GetRow(this string[] grid, int index) => grid[index];
 
         private delegate string Selector(string[] grid, int index);
-        private static (uint length, int index) GetMirror(string[] grid, Selector selector, bool isCol = false)
+        private static bool IsMirror(this string[] grid, Selector selector, int index, bool isCol = false, int gap = 0)
         {
-            (uint length, int index) mirror = new();
-
-            List<string> lines = [selector(grid, 0)];
-            bool isReversing = false;
-            int length = isCol ? grid[0].Length : grid.Length;
-            for (int i = 1; i < length; i++)
-            {
-                if (lines.Count == 0) break;
-                if (!isReversing)
-                {
-                    if (selector(grid, i) == lines[^1])
-                    {
-                        mirror.index = i;
-                        mirror.length = 1;
-                        lines.RemoveAt(lines.Count - 1);
-                        isReversing = true;
-                        continue;
-                    }
-                    lines.Add(selector(grid, i));
-                }
-                else
-                {
-                    if (selector(grid, i) != lines[^1])
-                    {
-                        mirror.index = 0;
-                        mirror.length = 0;
-                        break;
-                    }
-                    mirror.length++;
-                    lines.RemoveAt(lines.Count - 1);
-                }
-            }
-
-            return mirror;
-        }
-
-        private static bool HasOneSet(this BitArray arr)
-        {
-            int count = 0;
-            foreach (bool b in arr)
-                if (b) count++;
-
-            return count == 1 ? true : false;
+            if (index - gap < 0 || index + gap + 1 >= (isCol ? grid[0].Length : grid.Length)) return true;
+            if (selector(grid, index - gap) == selector(grid, index + 1 + gap))
+                return grid.IsMirror(selector, index, isCol, gap + 1);
+            return false;
         }
     }
 }
