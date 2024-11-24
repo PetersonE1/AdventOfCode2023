4b9b04b6efc6b0669d351612d8351e983c372eb5
CF-Code-Collective-Admin <cf-code-collective@outlook.com>
2024-11-24 02:10:12 -0500

    attempting to fix smudges, input is weird, not sure why

--- AdventOfCode2023/Days/Day13.cs
+++ AdventOfCode2023/Days/Day13.cs
@@ -8,33 +8,51 @@ namespace AdventOfCode2023.Days
     {
         public static void Run(string input)
         {
-            Console.WriteLine(Part1(input));
-            Console.WriteLine(Part2(input));
+            Console.WriteLine(Part1(input, out var cache));
+            Console.WriteLine(Part2(input, cache));
         }
 
         // Perfect reflection => One side of the reflection must touch a border
 
-        private static ulong Part1(string input)
+        private static ulong Part1(string input, out (int index, bool isCol)[] cache)
         {
             string[][] grids = input.Split("\r\n\r\n").Select(s => s.Split("\r\n")).ToArray();
 
             ulong count = 0;
             
+            cache = new(int index, bool isCol)[grids.Length];
+            int index = 0;
             foreach (string[] grid in grids)
             {
                 for (int i = 1; i < grid[0].Length; i++)
-                    if (grid.IsMirror(GetColumn, i - 1, true)) count += (uint)i;
+                    if (grid.IsMirror(GetColumn, i - 1, true)) { count += (uint)i; cache[index] = (i, true); }
                 
                 for (int i = 1; i < grid.Length; i++)
-                    if (grid.IsMirror(GetRow, i - 1)) count += 100 * (uint)i;
+                    if (grid.IsMirror(GetRow, i - 1)) { count += 100 * (uint)i; cache[index] = (i, false); }
+                index++;
             }
 
             return count;
         }
 
-        private static ulong Part2(string input)
+        private static ulong Part2(string input, (int index, bool isCol)[] cache)
         {
-            throw new NotImplementedException();
+            string[][] grids = input.Split("\r\n\r\n").Select(s => s.Split("\r\n")).ToArray();
+            ulong count = 0;
+
+            int index = 0;
+            foreach (string[] grid in grids)
+            {
+                for (int i = 1; i < grid[0].Length; i++)
+                    if (!cache.Contains((i, true)) && grid.IsMirrorWithSmudge(GetColumn, i - 1, true)) {count += (uint)i; Console.WriteLine($"{index}: Mirror at column {i}");}
+                
+                for (int i = 1; i < grid.Length; i++)
+                    if (!cache.Contains((i, false)) && grid.IsMirrorWithSmudge(GetRow, i - 1)) {count += 100 * (uint)i; Console.WriteLine($"{index}: Mirror at row {i}");}
+
+                index++;
+            }
+
+            return count;
         }
 
         private static string GetColumn(this string[] grid, int index) => grid.Aggregate(string.Empty, (result, current) => result += current[index]);
@@ -48,6 +66,34 @@ namespace AdventOfCode2023.Days
             if (selector(grid, index - gap) == selector(grid, index + 1 + gap))
                 return grid.IsMirror(selector, index, isCol, gap + 1);
             return false;
+        }
+
+        private static bool IsMirrorWithSmudge(this string[] grid, Selector selector, int index, bool isCol = false, int gap = 0, bool smudgeFixed = false)
+        {
+            if (index - gap < 0 || index + gap + 1 >= (isCol ? grid[0].Length : grid.Length)) return true;
+            string s1 = selector(grid, index - gap);
+            string s2 = selector(grid, index + 1 + gap);
+            if (s1 == s2)
+                return grid.IsMirrorWithSmudge(selector, index, isCol, gap + 1, smudgeFixed);
+            if (!smudgeFixed && IsOneDifferent(s1, s2))
+            {
+                Console.WriteLine($"One different {(isCol ? "column" : "row")} at index {index} (gap {gap}):");
+                Console.WriteLine(s1);
+                Console.WriteLine(s2);
+                return grid.IsMirrorWithSmudge(selector, index, isCol, gap + 1, true);
+            }
+            return false;
+        }
+
+        private static bool IsOneDifferent(string a, string b)
+        {
+            if (a.Length != b.Length) return false;
+
+            int count = 0;
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] != b[i]) count++;
+
+            return count == 1;
         }
     }
 }
